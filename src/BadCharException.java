fefb816b033029678c6092cb8b0b617922653fcb
louis465 <louis__thie@hotmail.com>
2019-10-14 18:42:58 +0200

    Factorisation duel gam play ok Simplification Scanastringwth number ok Log Ok à completer

--- src/BadCharException.java
+++
@@ -1,5 +0,0 @@
-public class BadCharException extends Exception {
-
-        BadCharException () {
-        }
-    }
--- src/Main.java
+++
@@ -1,8 +0,0 @@
-    public class Main {
-
-        public static void main(String[] args) {
-            Gamechoice modeChoice = new Gamechoice();
-            modeChoice.runGame ();
-        }
-    }
-
